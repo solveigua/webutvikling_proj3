4c211d4d6d4117579f424aa99b2e28db593af6d3
Ingrid Marie Sørensen <ingrmsor@stud.ntnu.no>
2021-11-02 21:59:38 +0100

    renamed sortedmovies

--- marvel/src/components/Movies/MoviesContainer.tsx
+++ marvel/src/components/Movies/MoviesContainer.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { connect } from "react-redux";
-import classes from './SortedMovies.module.css';
+import classes from './MovieContainer.module.css';
 import MovieItem from './MovieItem/MovieItem';
 import { Movie } from '../Types';
 
