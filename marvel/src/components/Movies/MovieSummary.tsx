9dc1a879a25009823fd1d22c2371f2f7bd96b6df
Solveig Aune <solveig.aune@live.no>
2021-11-02 16:48:32 +0100

    #20 show all movies complete

--- marvel/src/components/Movies/MovieSummary.tsx
+++ marvel/src/components/Movies/MovieSummary.tsx
@@ -1,17 +1,59 @@
+import React, { Component } from 'react';
 import SortingChart from '../UI/SortingChart';
 import SortingSurvey from '../UI/SortingSurvey';
 import classes from './MovieSummary.module.css';
+import {  fetchMovies } from '../../actions/searchActions';
+import { connect, Provider } from 'react-redux';
+import store from '../../store';
 
-//informaton only, can add more here
+interface IAllMoviesProps {
+    fetchMovies: any; /*TYPE? fetchMovies ligger i searchActions*/
+    }
+    
+  interface IAllMoviesState {
+    moviesList: { _id: number, title: string, seqNr: number, releaseYear: number, rating: number, __typename: string }[];
+    }
 
-const MovieSummary = () => {
-    return (
-    <section className={classes.summary}>
-        <h2>All Marvel movies</h2>
-        <p> Search for you favourite Marvel movie</p>
-        <SortingChart/>
-    </section>
-    );
+export class MovieSummary extends Component<IAllMoviesProps, IAllMoviesState> {
+
+    state: IAllMoviesState = {
+        moviesList: []
+      };
+
+    showAllMovies = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        this.props.fetchMovies(" ");
+        this.setState({
+            moviesList: this.props.fetchMovies(" ")
+          });
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+            <section className={classes.summary}>
+                <h2>Marvel movies</h2>
+                <p>
+                    Search for your favourite Marvel movie!
+            <br />
+            Give the movies a rating of your choice.
+        </p>
+                <SortingChart />
+                <div className={classes.buttoncontainer}>
+                    <button className={classes.button} onClick={this.showAllMovies}> Show All Movies</button>
+                </div>
+            </section>
+            </Provider>
+        );
+    }
 };
 
-export default MovieSummary;
+const mapStateToProps = (state: any) /*TYPE?*/ => ({
+    text: state.movies.text
+})
+
+export default connect(
+    mapStateToProps,
+    {fetchMovies}
+    )(MovieSummary);
+
